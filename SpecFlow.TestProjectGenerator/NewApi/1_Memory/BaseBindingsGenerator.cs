be374c3029eb55f1c595dcca9a93c1ba82e29646
David Eiwen <david-e-winlive@live.at>
2018-04-16 15:15:09 +0200

    Added initial logic for generating binding files

---
+++ SpecFlow.TestProjectGenerator/NewApi/1_Memory/BaseBindingsGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
+{
+    public abstract class BaseBindingsGenerator
+    {
+        public abstract ProjectFile GenerateBindingClass(string name, string content);
+
+        public abstract ProjectFile GenerateBindingMethod(string method);
+    }
+}
---
+++ SpecFlow.TestProjectGenerator/NewApi/1_Memory/CSharpBindingsGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
+{
+    public class CSharpBindingsGenerator : BaseBindingsGenerator
+    {
+        private const string BindingsClassTemplate = @"
+using TechTalk.SpecFlow;
+public class {0}
+{{
+    {1}
+}}";
+
+        public override ProjectFile GenerateBindingClass(string name, string content)
+        {
+            return new ProjectFile(name, "Compile", content);
+        }
+
+        public override ProjectFile GenerateBindingMethod(string method)
+        {
+            string randomClassName = $"BindingsClass_{Guid.NewGuid():N}";
+            return new ProjectFile($"{randomClassName}.cs", "Compile", string.Format(BindingsClassTemplate, randomClassName, method));
+        }
+    }
+}
