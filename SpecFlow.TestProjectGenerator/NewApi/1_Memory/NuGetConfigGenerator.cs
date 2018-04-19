0572a0f66d95e22755223976c415d08571b7a92c
David Eiwen <david-e-winlive@live.at>
2018-04-19 10:54:31 +0200

    Refactored NuGetConfigGenerator
    resolves #6

--- SpecFlow.TestProjectGenerator/NewApi/1_Memory/NuGetConfigGenerator.cs
+++ SpecFlow.TestProjectGenerator/NewApi/1_Memory/NuGetConfigGenerator.cs
@@ -18,31 +18,51 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
 
                     writer.WriteStartElement("packageSources");
 
-                    foreach (var source in nuGetSources ?? new NuGetSource[0])
-                    {
-                        writer.WriteStartElement("add");
-                        writer.WriteAttributeString("key", source.Key);
-                        writer.WriteAttributeString("value", source.Value);
-                        writer.WriteEndElement();
-                    }
-                    
-                    writer.WriteStartElement("add");
-                    writer.WriteAttributeString("key", "Nuget.org");
-                    writer.WriteAttributeString("value", "https://api.nuget.org/v3/index.json");
-                    writer.WriteEndElement();
+                    WriteNuGetSources(writer, nuGetSources);
+
+                    WriteNuGetOrgSource(writer);
 
                     writer.WriteEndElement();
 
                     writer.WriteEndElement();
                     writer.Flush();
 
-                    ms.Seek(0, SeekOrigin.Begin);
+                    return GenerateProjectFile(ms);
 
-                    using (var sr = new StreamReader(ms))
-                    {
-                        return new ProjectFile("nuget.config", "None", sr.ReadToEnd());
-                    }
                 }
+            }
+        }
+
+        public void WriteNuGetSources(XmlWriter writer, NuGetSource[] nuGetSources)
+        {
+            foreach (var source in nuGetSources ?? new NuGetSource[0])
+            {
+                WriteNuGetSource(writer, source);
+            }
+        }
+
+        public void WriteNuGetSource(XmlWriter writer, NuGetSource nuGetSource)
+        {
+            writer.WriteStartElement("add");
+            writer.WriteAttributeString("key", nuGetSource.Key);
+            writer.WriteAttributeString("value", nuGetSource.Value);
+            writer.WriteEndElement();
+        }
+
+        public void WriteNuGetOrgSource(XmlWriter writer)
+        {
+            var nuGetOrg = new NuGetSource("Nuget.org", "https://api.nuget.org/v3/index.json");
+
+            WriteNuGetSource(writer, nuGetOrg);
+        }
+
+        public ProjectFile GenerateProjectFile(MemoryStream ms)
+        {
+            ms.Seek(0, SeekOrigin.Begin);
+
+            using (var sr = new StreamReader(ms))
+            {
+                return new ProjectFile("nuget.config", "None", sr.ReadToEnd());
             }
         }
     }
