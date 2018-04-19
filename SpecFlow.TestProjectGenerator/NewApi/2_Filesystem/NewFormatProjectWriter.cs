da649e23cf46b259337daed406af5777bfc3ade3
David Eiwen <david-e-winlive@live.at>
2018-04-19 10:43:16 +0200

    Refactored NewFormatProjectWriter
    resolves #9

--- SpecFlow.TestProjectGenerator/NewApi/2_Filesystem/NewFormatProjectWriter.cs
+++ SpecFlow.TestProjectGenerator/NewApi/2_Filesystem/NewFormatProjectWriter.cs
@@ -3,41 +3,64 @@ using System.IO;
 using System.Xml;
 using SpecFlow.TestProjectGenerator.NewApi._1_Memory;
 using SpecFlow.TestProjectGenerator.NewApi._1_Memory.Extensions;
+using SpecFlow.TestProjectGenerator.NewApi._2_Filesystem.Commands;
 using SpecFlow.TestProjectGenerator.NewApi._2_Filesystem.Commands.Dotnet;
 
 namespace SpecFlow.TestProjectGenerator.NewApi._2_Filesystem
 {
     public class NewFormatProjectWriter : BaseProjectWriter
     {
-        public override string WriteProject(Project project, string path)
+        public override string WriteProject(Project project, string projRootPath)
         {
             if (project is null)
             {
                 throw new ArgumentNullException(nameof(project));
             }
 
-            string template = project.ProjectType == ProjectType.Exe ? "console" : "classlib";
-
-            var newProjCommand = DotNet.New()
-                                       .Project()
-                                       .InFolder(path)
-                                       .WithName(project.Name)
-                                       .UsingTemplate(template)
-                                       .WithLanguage(project.ProgrammingLanguage)
-                                       .Build();
-
-            var newProjResult = newProjCommand.Execute();
-
-            if (newProjResult.ExitCode != 0)
-            {
-                throw new ProjectCreationNotPossibleException();
-            }
+            // dotnet new
+            CreateProjectFile(project, projRootPath);
 
             string projFileName = $"{project.Name}.{project.ProgrammingLanguage.ToProjectFileExtension()}";
 
-            string projFilePath = Path.Combine(path, projFileName);
+            string projFilePath = Path.Combine(projRootPath, projFileName);
 
             // nuget packages
+            WriteNuGetPackages(project, projFilePath);
+
+            // p2p references
+            WriteProjectReferences(project, projFilePath);
+
+            var doc = new XmlDocument();
+            doc.Load(projFilePath);
+            var projRootNode = doc.SelectSingleNode("//Project") ?? throw new ProjectCreationNotPossibleException("Project root node could not be found in project file.");
+
+            // write GAC references
+            WriteReferences(project, doc, projRootNode);
+
+            // set target framework moniker
+            SetTargetFramework(project, doc, projRootNode);
+
+            doc.Save(projFilePath);
+
+            // write project files
+            WriteProjectFiles(project, projRootPath);
+
+            return projFilePath;
+        }
+
+        public CommandResult ExecuteCommandBuilder(CommandBuilder cb, Exception ex)
+        {
+            var result = cb.Execute();
+
+            if (result.ExitCode != 0)
+            {
+                throw ex;
+            }
+            return result;
+        }
+
+        public void WriteNuGetPackages(Project project, string projFilePath)
+        {
             foreach (var nugetPackage in project.NuGetPackages)
             {
                 var addPackageCommand = DotNet.Add()
@@ -46,13 +69,14 @@ namespace SpecFlow.TestProjectGenerator.NewApi._2_Filesystem
                                               .WithPackageVersion(nugetPackage.Version)
                                               .ToProject(projFilePath)
                                               .Build();
-                if (addPackageCommand.Execute().ExitCode != 0)
-                {
-                    throw new ProjectCreationNotPossibleException();
-                }
+                ExecuteCommandBuilder(
+                    addPackageCommand,
+                    new ProjectCreationNotPossibleException($"Adding nuGet Package '{nugetPackage.Name}', {nugetPackage.Version} failed."));
             }
+        }
 
-            // p2p references
+        public void WriteProjectReferences(Project project, string projFilePath)
+        {
             foreach (var projReference in project.ProjectReferences)
             {
                 var addReferenceCommand = DotNet.Add()
@@ -61,16 +85,39 @@ namespace SpecFlow.TestProjectGenerator.NewApi._2_Filesystem
                                                 .ToProject(projFilePath)
                                                 .Build();
 
-                if (addReferenceCommand.Execute().ExitCode != 0)
-                {
-                    throw new ProjectCreationNotPossibleException();
-                }
+                ExecuteCommandBuilder(
+                    addReferenceCommand,
+                    new ProjectCreationNotPossibleException($"Adding refence to '{projReference.Path}' failed."));
             }
+        }
 
-            var doc = new XmlDocument();
-            doc.Load(projFilePath);
-            var projRoot = doc.SelectSingleNode("//Project") ?? throw new ProjectCreationNotPossibleException();
+        public void WriteProjectFiles(Project project, string projRootPath)
+        {
+            var fileWriter = new ProjectFileWriter();
+            foreach (var file in project.Files)
+            {
+                fileWriter.Write(file, projRootPath);
+            }
+        }
 
+        public void SetTargetFramework(Project project, XmlDocument doc, XmlNode projRootNode)
+        {
+            var targetFrameworkNode =
+                projRootNode.SelectSingleNode("//PropertyGroup/TargetFramework")
+                ?? throw new ProjectCreationNotPossibleException();
+
+            string newTargetFrameworks = project.TargetFrameworks.ToTargetFrameworkMoniker();
+            var targetFrameworkParentGroup =
+                targetFrameworkNode.ParentNode ?? throw new ProjectCreationNotPossibleException();
+            var multipleTargetFrameworksNode = doc.CreateElement("TargetFrameworks");
+            multipleTargetFrameworksNode.InnerText = newTargetFrameworks;
+
+            targetFrameworkParentGroup.RemoveChild(targetFrameworkNode);
+            targetFrameworkParentGroup.AppendChild(multipleTargetFrameworksNode);
+        }
+
+        public void WriteReferences(Project project, XmlDocument doc, XmlNode projRootNode)
+        {
             // GAC and library references cannot be added in new Csproj format (via dotnet CLI)
             // see https://github.com/dotnet/sdk/issues/987
             // Therefore, write them manually into the project file
@@ -85,32 +132,25 @@ namespace SpecFlow.TestProjectGenerator.NewApi._2_Filesystem
                     referencesNode.AppendChild(node);
                 }
 
-                projRoot.AppendChild(referencesNode);
+                projRootNode.AppendChild(referencesNode);
             }
+        }
 
-            // set target framework moniker
-            var targetFrameworkNode =
-                projRoot.SelectSingleNode("//PropertyGroup/TargetFramework") ?? throw new ProjectCreationNotPossibleException();
+        public void CreateProjectFile(Project project, string projRootPath)
+        {
+            string template = project.ProjectType == ProjectType.Exe ? "console" : "classlib";
 
-            string newTargetFrameworks = project.TargetFrameworks.ToTargetFrameworkMoniker();
-            var targetFrameworkParentGroup =
-                targetFrameworkNode.ParentNode ?? throw new ProjectCreationNotPossibleException();
-            var multipleTargetFrameworksNode = doc.CreateElement("TargetFrameworks");
-            multipleTargetFrameworksNode.InnerText = newTargetFrameworks;
+            var newProjCommand = DotNet.New()
+                                       .Project()
+                                       .InFolder(projRootPath)
+                                       .WithName(project.Name)
+                                       .UsingTemplate(template)
+                                       .WithLanguage(project.ProgrammingLanguage)
+                                       .Build();
 
-            targetFrameworkParentGroup.RemoveChild(targetFrameworkNode);
-            targetFrameworkParentGroup.AppendChild(multipleTargetFrameworksNode);
-            
-            doc.Save(projFilePath);
-
-            // write project files
-            var fileWriter = new ProjectFileWriter();
-            foreach (var file in project.Files)
-            {
-                fileWriter.Write(file, path);
-            }
-
-            return projFilePath;
+            ExecuteCommandBuilder(
+                newProjCommand,
+                new ProjectCreationNotPossibleException("Execution of dotnet new failed."));
         }
     }
 }
