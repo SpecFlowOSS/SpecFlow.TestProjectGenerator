236e825e4a64c91de4404222bd79134e46c16f4e
David Eiwen <david-e-winlive@live.at>
2018-04-19 11:20:25 +0200

    Refactored AppConfigGenerator
    resolves #4

--- SpecFlow.TestProjectGenerator/NewApi/1_Memory/AppConfigGenerator.cs
+++ SpecFlow.TestProjectGenerator/NewApi/1_Memory/AppConfigGenerator.cs
@@ -8,7 +8,11 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
 {
     public class AppConfigGenerator
     {
-        public ProjectFile Generate(string unitTestProvider, StepAssembly[] stepAssemblies = null, SpecFlowPlugin[] plugins = null, CultureInfo featureLanguage = null)
+        public ProjectFile Generate(
+            string unitTestProvider,
+            StepAssembly[] stepAssemblies = null,
+            SpecFlowPlugin[] plugins = null,
+            CultureInfo featureLanguage = null)
         {
             featureLanguage = featureLanguage ?? CultureInfo.GetCultureInfo("en-US");
 
@@ -18,62 +22,104 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
                 {
                     writer.WriteStartElement("configuration");
 
-                    writer.WriteStartElement("specFlow");
-
-                    writer.WriteStartElement("unitTestProvider");
-                    writer.WriteAttributeString("name", unitTestProvider);
-                    writer.WriteEndElement();
-
-                    writer.WriteStartElement("language");
-                    writer.WriteAttributeString("feature", featureLanguage.Name);
-                    writer.WriteEndElement();
-
-                    if (!(stepAssemblies is null))
-                    {
-                        writer.WriteStartElement("stepAssemblies");
-                        foreach (var stepAssembly in stepAssemblies)
-                        {
-                            writer.WriteStartElement("stepAssembly");
-                            writer.WriteAttributeString("assembly", stepAssembly.Assembly);
-                            writer.WriteEndElement();
-                        }
-
-                        writer.WriteEndElement();
-                    }
-
-                    if (!(plugins is null))
-                    {
-                        writer.WriteStartElement("plugins");
-                        foreach (var plugin in plugins)
-                        {
-                            writer.WriteStartElement("add");
-                            writer.WriteAttributeString("name", plugin.Name);
-
-                            if(!string.IsNullOrEmpty(plugin.Path))
-                            writer.WriteAttributeString("path", plugin.Path);
-
-                            if(plugin.Type != (SpecFlowPluginType.Generator | SpecFlowPluginType.Runtime))
-                            writer.WriteAttributeString("type", plugin.Type.ToPluginTypeString());
-
-
-                            writer.WriteEndElement();
-                        }
-
-                        writer.WriteEndElement();
-                    }
-
-                    writer.WriteEndElement();
+                    WriteSpecFlow(writer, unitTestProvider, stepAssemblies, plugins, featureLanguage);
 
                     writer.WriteEndElement();
                     writer.Flush();
 
-                    ms.Seek(0, SeekOrigin.Begin);
-
-                    using (var sr = new StreamReader(ms))
-                    {
-                        return new ProjectFile("app.config", "None", sr.ReadToEnd());
-                    }
+                    return GenerateProjectFile(ms);
                 }
+            }
+        }
+
+        public void WriteSpecFlow(
+            XmlWriter writer, 
+            string unitTestProvider,
+            StepAssembly[] stepAssemblies = null,
+            SpecFlowPlugin[] plugins = null,
+            CultureInfo featureLanguage = null)
+        {
+            writer.WriteStartElement("specFlow");
+
+            WriteUnitTestProvider(writer, unitTestProvider);
+
+            WriteLanguage(writer, featureLanguage);
+
+            WriteStepAssemblies(writer, stepAssemblies);
+
+            WritePlugins(writer, plugins);
+
+            writer.WriteEndElement();
+        }
+
+        public void WriteUnitTestProvider(XmlWriter writer, string unitTestProvider)
+        {
+            writer.WriteStartElement("unitTestProvider");
+            writer.WriteAttributeString("name", unitTestProvider);
+            writer.WriteEndElement();
+        }
+
+        public void WriteLanguage(XmlWriter writer, CultureInfo featureLanguage)
+        {
+            writer.WriteStartElement("language");
+            writer.WriteAttributeString("feature", featureLanguage.Name);
+            writer.WriteEndElement();
+        }
+
+        public void WriteStepAssemblies(XmlWriter writer, StepAssembly [] stepAssemblies)
+        {
+            if (stepAssemblies is null) return;
+            writer.WriteStartElement("stepAssemblies");
+            foreach (var stepAssembly in stepAssemblies)
+            {
+                WriteStepAssembly(writer, stepAssembly);
+            }
+
+            writer.WriteEndElement();
+
+        }
+
+        public void WriteStepAssembly(XmlWriter writer, StepAssembly stepAssembly)
+        {
+            writer.WriteStartElement("stepAssembly");
+            writer.WriteAttributeString("assembly", stepAssembly.Assembly);
+            writer.WriteEndElement();
+        }
+
+        public void WritePlugins(XmlWriter writer, SpecFlowPlugin[] plugins)
+        {
+            if (plugins is null) return;
+            writer.WriteStartElement("plugins");
+            foreach (var plugin in plugins)
+            {
+                WritePlugin(writer, plugin);
+            }
+
+            writer.WriteEndElement();
+        }
+
+        public void WritePlugin(XmlWriter writer, SpecFlowPlugin plugin)
+        {
+            writer.WriteStartElement("add");
+            writer.WriteAttributeString("name", plugin.Name);
+
+            if (!string.IsNullOrEmpty(plugin.Path))
+                writer.WriteAttributeString("path", plugin.Path);
+
+            if (plugin.Type != (SpecFlowPluginType.Generator | SpecFlowPluginType.Runtime))
+                writer.WriteAttributeString("type", plugin.Type.ToPluginTypeString());
+
+
+            writer.WriteEndElement();
+        }
+
+        public ProjectFile GenerateProjectFile(MemoryStream ms)
+        {
+            ms.Seek(0, SeekOrigin.Begin);
+
+            using (var sr = new StreamReader(ms))
+            {
+                return new ProjectFile("app.config", "None", sr.ReadToEnd());
             }
         }
     }
