8eb4fe750cab9ea2690fd91f4c0e93e11711abc7
David Eiwen <david-e-winlive@live.at>
2018-04-19 11:08:11 +0200

    Refactored JsonConfigGenerator
    resolves #5

--- SpecFlow.TestProjectGenerator/NewApi/1_Memory/JsonConfigGenerator.cs
+++ SpecFlow.TestProjectGenerator/NewApi/1_Memory/JsonConfigGenerator.cs
@@ -9,8 +9,6 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
     {
         public ProjectFile Generate(string unitTestProvider = null, StepAssembly[] stepAssemblies = null, SpecFlowPlugin[] plugins = null, CultureInfo featureLanguage = null)
         {
-            featureLanguage = featureLanguage ?? CultureInfo.GetCultureInfo("en-US");
-
             using (var stringWriter = new StringWriter())
             {
                 using (var jsonWriter = new JsonTextWriter(stringWriter))
@@ -18,87 +16,7 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
                     // open root object
                     jsonWriter.WriteStartObject();
 
-                    // open specflow object
-                    jsonWriter.WritePropertyName("specFlow");
-                    jsonWriter.WriteStartObject();
-
-                    // open unitTestProvider object
-                    jsonWriter.WritePropertyName("unitTestProvider");
-
-                    jsonWriter.WriteStartObject();
-                    jsonWriter.WritePropertyName("name");
-                    jsonWriter.WriteValue(unitTestProvider);
-
-                    // close unitTestProvider
-                    jsonWriter.WriteEndObject();
-
-                    // open language object
-                    jsonWriter.WritePropertyName("language");
-                    jsonWriter.WriteStartObject();
-
-                    jsonWriter.WritePropertyName("feature");
-                    jsonWriter.WriteValue(featureLanguage.Name);
-
-                    // close language object
-                    jsonWriter.WriteEndObject();
-
-                    if (!(stepAssemblies is null))
-                    {
-                        // open stepAssemblies array
-                        jsonWriter.WritePropertyName("stepAssemblies");
-                        jsonWriter.WriteStartArray();
-
-                        foreach (var stepAssembly in stepAssemblies)
-                        {
-                            // open stepAssembly object
-                            jsonWriter.WriteStartObject();
-
-                            jsonWriter.WritePropertyName("assembly");
-                            jsonWriter.WriteValue(stepAssembly.Assembly);
-
-                            // clase stepAssembly object
-                            jsonWriter.WriteEndObject();
-                        }
-
-                        // close stepAssemblies array
-                        jsonWriter.WriteEndArray();
-                    }
-
-                    if (!(plugins is null))
-                    {
-                        // open plugins array
-                        jsonWriter.WritePropertyName("plugins");
-                        jsonWriter.WriteStartArray();
-
-                        foreach (var plugin in plugins)
-                        {
-                            // open add object
-                            jsonWriter.WriteStartObject();
-                            jsonWriter.WritePropertyName("name");
-                            jsonWriter.WriteValue(plugin.Name);
-
-                            if (!string.IsNullOrEmpty(plugin.Path))
-                            {
-                                jsonWriter.WritePropertyName("path");
-                                jsonWriter.WriteValue(plugin.Path);
-                            }
-
-                            if (plugin.Type != (SpecFlowPluginType.Generator | SpecFlowPluginType.Runtime))
-                            {
-                                jsonWriter.WritePropertyName("type");
-                                jsonWriter.WriteValue(plugin.Type.ToPluginTypeString());
-                            }
-
-                            // close add object
-                            jsonWriter.WriteEndObject();
-                        }
-
-                        // close plugins array
-                        jsonWriter.WriteEndArray();
-                    }
-                    
-                    // close specflow object
-                    jsonWriter.WriteEndObject();
+                    WriteSpecFlow(jsonWriter, unitTestProvider, stepAssemblies, plugins, featureLanguage);
 
                     // close root object
                     jsonWriter.WriteEndObject();
@@ -107,6 +25,123 @@ namespace SpecFlow.TestProjectGenerator.NewApi._1_Memory
                     return new ProjectFile("specflow.json", "None", stringWriter.ToString());
                 }
             }
+        }
+
+        public void WriteUnitTestProvider(JsonWriter jsonWriter, string unitTestProvider)
+        {
+            // open unitTestProvider object
+            jsonWriter.WritePropertyName("unitTestProvider");
+
+            jsonWriter.WriteStartObject();
+            jsonWriter.WritePropertyName("name");
+            jsonWriter.WriteValue(unitTestProvider);
+
+            // close unitTestProvider
+            jsonWriter.WriteEndObject();
+        }
+
+        public void WriteLanguage(JsonWriter jsonWriter, CultureInfo featureLanguage)
+        {
+            // open language object
+            jsonWriter.WritePropertyName("language");
+            jsonWriter.WriteStartObject();
+
+            jsonWriter.WritePropertyName("feature");
+            jsonWriter.WriteValue(featureLanguage.Name);
+
+            // close language object
+            jsonWriter.WriteEndObject();
+        }
+
+        public void WriteStepAssemblies(JsonWriter jsonWriter, StepAssembly[] stepAssemblies)
+        {
+            if (!(stepAssemblies is null))
+            {
+                // open stepAssemblies array
+                jsonWriter.WritePropertyName("stepAssemblies");
+                jsonWriter.WriteStartArray();
+
+                foreach (var stepAssembly in stepAssemblies)
+                {
+                    WriteStepAssembly(jsonWriter, stepAssembly);
+                }
+
+                // close stepAssemblies array
+                jsonWriter.WriteEndArray();
+            }
+        }
+
+        public void WriteStepAssembly(JsonWriter jsonWriter, StepAssembly stepAssembly)
+        {
+            // open stepAssembly object
+            jsonWriter.WriteStartObject();
+
+            jsonWriter.WritePropertyName("assembly");
+            jsonWriter.WriteValue(stepAssembly.Assembly);
+
+            // clase stepAssembly object
+            jsonWriter.WriteEndObject();
+        }
+
+        public void WritePlugins(JsonWriter jsonWriter, SpecFlowPlugin[] plugins)
+        {
+            if (plugins is null) return;
+
+            // open plugins array
+            jsonWriter.WritePropertyName("plugins");
+            jsonWriter.WriteStartArray();
+
+            foreach (var plugin in plugins)
+            {
+                WritePlugin(jsonWriter, plugin);
+            }
+
+            // close plugins array
+            jsonWriter.WriteEndArray();
+        }
+
+        public void WritePlugin(JsonWriter jsonWriter, SpecFlowPlugin plugin)
+        {
+            // open add object
+            jsonWriter.WriteStartObject();
+            jsonWriter.WritePropertyName("name");
+            jsonWriter.WriteValue(plugin.Name);
+
+            if (!string.IsNullOrEmpty(plugin.Path))
+            {
+                jsonWriter.WritePropertyName("path");
+                jsonWriter.WriteValue(plugin.Path);
+            }
+
+            if (plugin.Type != (SpecFlowPluginType.Generator | SpecFlowPluginType.Runtime))
+            {
+                jsonWriter.WritePropertyName("type");
+                jsonWriter.WriteValue(plugin.Type.ToPluginTypeString());
+            }
+
+            // close add object
+            jsonWriter.WriteEndObject();
+        }
+
+        public void WriteSpecFlow(JsonWriter jsonWriter, string unitTestProvider = null, StepAssembly[] stepAssemblies = null, SpecFlowPlugin[] plugins = null, CultureInfo featureLanguage = null)
+        {
+            featureLanguage = featureLanguage ?? CultureInfo.GetCultureInfo("en-US");
+            unitTestProvider = unitTestProvider ?? "MsTest";
+
+            // open specflow object
+            jsonWriter.WritePropertyName("specFlow");
+            jsonWriter.WriteStartObject();
+
+            WriteUnitTestProvider(jsonWriter, unitTestProvider);
+
+            WriteLanguage(jsonWriter, featureLanguage);
+
+            WriteStepAssemblies(jsonWriter, stepAssemblies);
+
+            WritePlugins(jsonWriter, plugins);
+
+            // close specflow object
+            jsonWriter.WriteEndObject();
         }
     }
 }
