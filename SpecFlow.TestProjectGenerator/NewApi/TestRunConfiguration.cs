dc2b2438c6f0386a60515951db234e547959a4f6
Andreas Willich <andreas.willich@techtalk.at>
2018-05-22 09:14:55 +0200

    fix parser tests

--- SpecFlow.TestProjectGenerator/NewApi/TestRunConfiguration.cs
+++ SpecFlow.TestProjectGenerator/NewApi/TestRunConfiguration.cs
@@ -4,14 +4,6 @@ namespace TechTalk.SpecFlow.TestProjectGenerator.NewApi
 {
     public class TestRunConfiguration
     {
-        public TestRunConfiguration(ProgrammingLanguage programmingLanguage, ProjectFormat projectFormat, TargetFramework targetFramework, UnitTestProvider unitTestProvider)
-        {
-            ProgrammingLanguage = programmingLanguage;
-            ProjectFormat = projectFormat;
-            TargetFramework = targetFramework;
-            UnitTestProvider = unitTestProvider;
-        }
-
         public ProgrammingLanguage ProgrammingLanguage { get; set; }
         public ProjectFormat ProjectFormat { get; set; }
 
