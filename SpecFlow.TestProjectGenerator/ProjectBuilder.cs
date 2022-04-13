2e41e142493de5452828f3e9aa79ab82fbd453a3
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-04-13 11:10:11 +0200

    Add System.Runtime.CompilerServices.Unsafe dependency for .NET 4.6.1 / old project format

--- SpecFlow.TestProjectGenerator/ProjectBuilder.cs
+++ SpecFlow.TestProjectGenerator/ProjectBuilder.cs
@@ -230,6 +230,7 @@ namespace TechTalk.SpecFlow.TestProjectGenerator
                 if (_project.ProjectFormat == ProjectFormat.Old)
                 {
                     _project.AddNuGetPackage("Google.Protobuf", "3.7.0", new NuGetPackageAssembly("Google.Protobuf, Version=3.7.0.0, Culture=neutral, PublicKeyToken=a7d26565bac4d604", "net45\\Google.Protobuf.dll"));
+                    _project.AddNuGetPackage("System.Runtime.CompilerServices.Unsafe", "6.0.0", new NuGetPackageAssembly("System.Runtime.CompilerServices.Unsafe, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", "net461\\System.Runtime.CompilerServices.Unsafe.dll"));
                 }
 
                 if (_currentVersionDriver.SpecFlowVersion >= new Version(3, 0))
